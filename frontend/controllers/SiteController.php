070392887e4d03a715e8254acca180ef5e5b6fb5
dencygb <dency@abacies.com>
2016-11-18 11:31:42 +0530

    authenticated index page

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -34,7 +34,7 @@ class SiteController extends Controller
                         'roles' => ['?'],
                     ],
                     [
-                        'actions' => ['logout'],
+                        'actions' => ['logout','index'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
