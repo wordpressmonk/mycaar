26ca8fa4b0c4650e2030c5011110d2e9d87523f8
Arivu-abacies <arivazhagan@abacies.com>
2016-11-22 16:58:05 +0530

    Enrolled True, False Changes

--- common/models/search/SearchEnrolment.php
+++ common/models/search/SearchEnrolment.php
@@ -20,8 +20,8 @@ class SearchEnrolment extends Enrolment
     public function rules()
     {
         return [
-               [['id','enrolled'], 'integer'],	
-			   [['enrolled','username'], 'string'],
+               [['id','isenrolled'], 'integer'],	
+			   [['username'], 'string'],
 			   [['username'], 'safe'],				   
         ];
     }
@@ -62,7 +62,7 @@ class SearchEnrolment extends Enrolment
         } 
 		$query->andFilterWhere([
             'id' => $this->id,
-            'enrolled' => $this->enrolled,          
+            'isenrolled' => $this->isenrolled,          
            
         ]);
         
