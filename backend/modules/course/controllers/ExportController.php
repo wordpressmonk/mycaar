9750e297400df21dc5d0bfa0f2d3f7af2f5a3d52
dencygb <dency@abacies.com>
2016-12-12 10:31:35 +0530

    space problem in filename

--- backend/modules/course/controllers/ExportController.php
+++ backend/modules/course/controllers/ExportController.php
@@ -387,7 +387,7 @@ class ExportController extends Controller
 		$objPHPExcel->setActiveSheetIndex(0);		
 		//save sheet
 		$objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
-		$categoryname = trim($categoryname);
+		$categoryname = preg_replace('/\s+/', '', $categoryname);
 		$objWriter->save( \Yii::$app->basePath.'/web/uploads/Program-'.str_replace('+', '_', urlencode($categoryname) ).date('y-m-d').'-Assessment-Report.xls' );
 		$file_url = \Yii::$app->homeurl.'uploads/Program-'.str_replace('+', '_', urlencode($categoryname) ).date('y-m-d').'-Assessment-Report.xls';
 		return $this->redirect($file_url);
