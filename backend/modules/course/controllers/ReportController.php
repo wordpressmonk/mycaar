c414587764b4ef26abbd07c785ec2d931cfbc651
dencygb <dency@abacies.com>
2016-12-29 13:46:58 +0530

    date calculation

--- backend/modules/course/controllers/ReportController.php
+++ backend/modules/course/controllers/ReportController.php
@@ -344,19 +344,31 @@ class ReportController extends Controller
 	/**
 	 * Auto-reset lesson after a particular time period
 	 */
-	public function actionAutoReset($unit_id){
-		//sample cron
+	public function actionAutoReset($unit_id,$months){
+		
+		$today = time();
+		$twoMonthsLater = strtotime("+{$months} months", $today);
+echo 'Today is : ' . date('m-d-Y', $today);
+echo 'Two months later will be : ' . date('m-d-Y', $twoMonthsLater);
+echo $month = (int)date('m', $twoMonthsLater);
+echo $date = (int)date('d', $twoMonthsLater);
+/* 		//sample cron
 		//cd /home/wordpressmonks/public_html/works/mycaar_lms && php yii archive/reports
 		if(Unit::findOne($unit_id) != null){
 			$output = shell_exec('crontab -l');
-			 $output = str_replace('* * * * * NEW_CRON'.PHP_EOL, "", $output);
-			 file_put_contents('/tmp/crontab.txt', $output);
-			/* file_put_contents('/tmp/crontab.txt', $output.'* * * * * cd /home/wordpressmonks/public_html/works/mycaar_lms && php yii reset/unit '.$unit_id.PHP_EOL); */
+			
+			//removing
+			// $output = str_replace('* * * * * NEW_CRON'.PHP_EOL, "", $output);
+			// file_put_contents('/tmp/crontab.txt', $output); 
+			 
+			 //adding
+			// file_put_contents('/tmp/crontab.txt', $output.'* * * * * cd /home/wordpressmonks/public_html/works/mycaar_lms && php yii reset/unit '.$unit_id.PHP_EOL); 
+			
 			echo exec('crontab /tmp/crontab.txt');			
 		}	 
 		//print all cron jobs
 		$output = shell_exec('crontab -l');
-		echo $output;
+		echo $output; */
 	}
 
 }
